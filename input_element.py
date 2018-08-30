aa5a594faade088db4a3c7a701e16dfe519cb609
Netdigger <netdigger@gmail.com>
2018-08-30 13:46:58 +1200

    fix InputElement: file path error

--- input_element.py
+++ input_element.py
@@ -5,12 +5,12 @@ from start_action import StartAction
 from line_action import LineAction
 from end_action import EndAction
 
+
 class InputElement:
 
     def __init__(self, directory, ext):
         self.dir = directory
         self.ext = ext
-
 
     def SetNextElement(self, out):
         self.out = out
@@ -28,10 +28,9 @@ class InputElement:
 
     def _Read(self, dirpath, name):
         print("Convert " + dirpath + name + " ...")
-        with open(dirpath + name) as fp:
+        with open(dirpath + "/" + name) as handle:
             start = StartAction(dirpath, name)
             self.out.Execute(start)
-            for line in fp:
+            for line in handle:
                 self.out.Execute(LineAction(line))
             self.out.Execute(EndAction())
-
